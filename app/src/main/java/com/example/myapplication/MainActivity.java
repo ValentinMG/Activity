64e1d76836c768aecf62d9467885cb1ca8891d6c
Valentin <rubenvale2601@gmail.com>
2021-09-17 13:27:03 -0500

    version inicial

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_suma;
+    private Button btn_resta;
+    private Button btn_multiplicacion;
+    private Button btn_division;
+
+    private TextView text_respuesta;
+    private EditText edit_numero_uno;
+    private EditText edit_numero_dos;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_suma = findViewById(R.id.button_suma);
+        btn_suma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                suma(Integer.parseInt(edit_numero_uno.getText().toString()), Integer.parseInt(edit_numero_dos.getText().toString()) );
+            }
+        });
+        btn_resta = findViewById(R.id.button_resta);
+
+
+        btn_multiplicacion = findViewById(R.id.button_multiplicacion);
+        btn_division = findViewById(R.id.button_division);
+
+        text_respuesta = findViewById(R.id.respuesta);
+
+        edit_numero_uno = findViewById(R.id.numero1);
+        edit_numero_dos = findViewById(R.id.numero2);
+
+    }
+    public int suma (int a,int b){
+        return a+b;
+    }
+    public int resta (int a,int b){
+        return a-b;
+    }
+    public int multiplicacion (int a,int b){
+        return a*b;
+    }
+    public int division (int a,int b){
+        return a/b;
+    }
+}
